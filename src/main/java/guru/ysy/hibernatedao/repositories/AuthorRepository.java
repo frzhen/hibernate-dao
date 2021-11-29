2dc90c1a95a2c3e83cfb1d1a04881e54e6b452c0
Fred Zhen <13911239075@163.com>
2021-11-29 14:54:55 +0800

    Modified repository query with Optional query, and added orElseThrow() to catch the optional return type with EntityNotFoundException, test succeed

--- src/main/java/guru/ysy/hibernatedao/repositories/AuthorRepository.java
+++ src/main/java/guru/ysy/hibernatedao/repositories/AuthorRepository.java
@@ -3,10 +3,12 @@ package guru.ysy.hibernatedao.repositories;
 import guru.ysy.hibernatedao.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * Created by zhenrui on 2021/11/28 11:50
  */
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
-    Author findAuthorByFirstNameAndLastName(String firstName, String lastName);
+    Optional<Author> findAuthorByFirstNameAndLastName(String firstName, String lastName);
 }
